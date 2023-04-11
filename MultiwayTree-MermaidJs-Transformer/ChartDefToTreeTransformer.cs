b23cdd10a5c94df621131485ac52142ec8b42da2
Steven Bass <stevenwbass@gmail.com>
2023-04-11 13:48:00 -0400

    stubs

---
+++ MultiwayTree-MermaidJs-Transformer/ChartDefToTreeTransformer.cs
@@ -0,0 +1,17 @@
+﻿using MultiwayTree;
+using System.Text;
+
+namespace MultiwayTreeMermaidJsTransformer
+{
+    public class ChartDefToTreeTransformer<T> where T : IEquatable<T>
+    {
+        public MultiwayTree<T> Transform(string chartDef)
+        {
+            var tree = new MultiwayTree<T>(default(T));
+
+            // TODO: parse chartDef and build tree
+
+            return tree;
+        }
+    }
+}
---
+++ MultiwayTree-MermaidJs-Transformer/TreeToChartDefTransformer.cs
@@ -0,0 +1,17 @@
+﻿using MultiwayTree;
+using System.Text;
+
+namespace MultiwayTreeMermaidJsTransformer
+{
+    public class TreeToChartDefTransformer<T> where T : IEquatable<T>
+    {
+        public string Transform(MultiwayTree<T> tree)
+        {
+            var sb = new StringBuilder();
+
+            // TODO: traverse tree and transform into mermaid chart def
+
+            return sb.ToString();
+        }
+    }
+}
